6bd3acd547fa7a8f4a5482c31be485fcdf25e2e4
Josh Burnwatt <burnwattj@gmail.com>
2019-06-25 10:40:00 -0500

    Add session and user backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root "static_pages#root"
   
   namespace :api, defaults: {format: :json} do
-
+    resource :user, only: [:create, :destroy, :show]
+    resource :session, only: [:create, :destroy]
   end
+  
 end
