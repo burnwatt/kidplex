199a34ac6e89249c8fcac215cd9ba8107128b35d
Josh Burnwatt <burnwattj@gmail.com>
2019-06-25 11:55:54 -0500

    Add root page and controller with basic rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root "static_pages#root"
+  root to:"static_pages#root"
   
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create, :destroy, :show]
+    resources :user, only: [:create, :show]
     resource :session, only: [:create, :destroy]
   end
-  
+
 end
