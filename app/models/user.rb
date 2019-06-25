742ba452f1d38cd63b643f72c9186ffe3eed4457
Josh Burnwatt <burnwattj@gmail.com>
2019-06-25 10:36:50 -0500

    Implement user model and application controller functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,49 @@
 #
 
 class User < ApplicationRecord
-    before_validation: ensure_session_token
+    attr_reader :password
     validates :username, :email, :session_token, :password_digest, presence: true
     validates :username, :email, uniqueness: true
     validates :password, length: { minimum: 8 }, allow_nil: true
+    before_validation: ensure_session_token
+    
+    # Associations for videos/lists
+    # 
+    # 
+    # 
 
     def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user && user.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.new_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token || self.reset_session_token!
+    end
+
+    def new_token
+        self.session_token = SecureRandom.urlsafe_base64
+        while User.find_by( session_token: self.session_token)
+            self.session_token = SecureRandom.urlsafe_base64
+        end
+        self.session_token
     end
 
 end
