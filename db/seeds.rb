537ad4a18cbc65a1132d4508bb4b8de7b323e0aa
Josh Burnwatt <burnwattj@gmail.com>
2019-06-26 14:46:48 -0500

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+woody = User.create(username: 'woody', email: 'woody@andysroom.com', password:'4SnakeInMyBoots!');
+buzz = User.create(username: 'buzz', email: 'buzz@andysroom.com', password:'2Infinity&Beyond!');
+demo = User.create(username: 'demo', email: 'demo@kidplex.com', password:'Movies123!');
